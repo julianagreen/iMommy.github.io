9404ce943f53955a6a53bf35de004f46fc05ad59
Erica Prenga <prenga@adobe.com>
2016-08-07 20:39:30 -0700

    fixed signin-signout process and got calendar events working

--- imommy.js
+++ imommy.js
@@ -2,14 +2,12 @@ var auth2; // The Sign-In object.
 var SCOPE = 'profile email https://www.googleapis.com/auth/calendar';
 var CLIENT_ID = '463068943114-9jvudf1fa16lljeuphr8oiak619elmmu.apps.googleusercontent.com';
 var API_KEY = 'AIzaSyDSg6Rg0QO3PXM4eOnwgY2I6zMxNsz-URE';
-var signinButton = document.getElementById('signin-button');
-var signoutButton = document.getElementById('signout-button');
-var calendarButton = document.getElementById('load-calendar');
+var signinButtonT
+var signoutButton 
+var calendarButton
 
 window.onload = function() {
-  // checkSignedIn();
   gapi.load('client:auth2', initAuth);
-  $('#save_button').on('click', onSave);
 }
 
 function initAuth() { // Log user in
@@ -21,32 +19,22 @@ function initAuth() { // Log user in
     auth2 = gapi.auth2.getAuthInstance();
     auth2.isSignedIn.listen(updateSigninStatus); // Listen for sign-in state changes.
     updateSigninStatus(auth2.isSignedIn.get()); // Handle the initial sign-in state.
-    signinButton.onclick = signIn;
-    signoutButton.onclick = signOut;
-    calendarButton.onclick = loadCalendarApi;
   });
 }
 
-function updateSigninStatus(isSignedIn) { //Switch button showing depending on user signin
-  if (isSignedIn) {
-    signinButton.style.display = 'none';
-    signoutButton.style.display = 'block';
-  } else {
-    signinButton.style.display = 'block';
-    signoutButton.style.display = 'none';
+function updateSigninStatus(isSignedIn) { //Switch current page depending on user signin
+  var location = window.location.pathname;
+  if (location == '/sign_in.html' && isSignedIn) {
+    window.location.href = '/imommy.html';
+  }
+  else if (location != '/sign_in.html' && !isSignedIn) {
+    window.location.href = '/sign_in.html';
   }
 }
 
-// function checkSignedIn(){
-//   var isSignedIn = auth2.isSignedIn.get();
-//   var location = window.location.href;
-//   if (location == 'http://localhost:8000/sign_in.html' && isSignedIn) {
-//     window.location.href = 'http://localhost:8000/imommy.html';
-//   }
-//   else if (location != 'http://localhost:8000/sign_in.html' && !isSignedIn) {
-//     window.location.href = 'http://localhost:8000/sign_in.html';
-//   }
-// }
+function checkSignedIn(){
+
+}
 
 function renderButton() {
   gapi.signin2.render('my-signin2', {
@@ -96,6 +84,54 @@ function listCalendars() { // List all calendars
   });
 }
 
+function addCalendarEvent(){
+    var details = {}
+  var list = $('.edit_cal input');
+  for (i = 0; i < list.length; i++) { 
+    details[list[i].id] = list[i].value;
+  }
+
+  var calendar_event = {
+    'summary': details.title,
+    'description': details.description,
+    "start": {
+      "dateTime": "2016-06-03T10:00:00.000-07:00",
+      "timeZone": "America/Los_Angeles"
+    },
+    "end": {
+      "dateTime": "2016-06-03T10:25:00.000-07:00",
+      "timeZone": "America/Los_Angeles"
+    },
+    'reminders': {
+      'useDefault': true,
+      // 'overrides': [
+      //   {'method': 'email', 'minutes': 24 * 60},
+      //   {'method': 'popup', 'minutes': 10}
+      // ]
+    }
+  };
+
+  var request = gapi.client.calendar.events.insert({
+    'calendarId': 'primary',
+    'resource': calendar_event
+  });
+
+  request.execute(function(response) {
+    debugger;
+    var events = response.items;
+
+    for (i = 0; i < events.length; i++) {
+      var event = events[i];
+      var when = event.start.dateTime;
+      if (!when) {
+        when = event.start.date;
+      }
+      // appendPre(event.summary + ' (' + when + ')');
+      // appendPre('Event created: ' + response.htmlLink);
+    }
+  });
+}
+
 function listUpcomingEvents(calendarID){ // Print the summary and start datetime/date of the next ten events
   var calendarId = calendarID;
   var request_events = gapi.client.calendar.events.list({
@@ -126,47 +162,6 @@ function appendPre(message) { // Append a pre element to the body containing the
   var textContent = document.createTextNode(message + '\n');
   pre.appendChild(textContent);
 }
-
-function onSave(event){
-  var details = {}
-  var list = $('.edit_cal input');
-  for (i=0;i<list.length();i++) {
-    details[list[i].id] = list[i].value;
-  }
-
-  var calendar_event = {
-    'summary': details.title,
-    'description': details.description,
-    'start': {
-      'dateTime': details.start_time,
-    },
-    'end': {
-      'dateTime': details.end_time,
-    },
-    'reminders': {
-      'useDefault': true,
-      // 'overrides': [
-      //   {'method': 'email', 'minutes': 24 * 60},
-      //   {'method': 'popup', 'minutes': 10}
-      // ]
-    }
-  };
-
-  var request = gapi.client.calendar.events.insert({
-    'calendarId': 'primary',
-    'resource': calendar_event
-  });
-
-  request.execute(function(event) {
-    appendPre('Event created: ' + event.htmlLink);
-  });
-}
- // (when save is clicked)
-//make a variable called summary that stores the input of the summary text box
-//create an event on the event screen with certain color dot, and text summary var
-//make vars that store the other inputs
-//create a calendar event with summary: summary, default reminder, and start time: start_time, end time: end_time.
-
 
 
 //events page on load
